2b0403ef7947747957c13d148731ed0c28c7dc7d
weiyuan12 <weiyuan.tan0@gmail.com>
2023-01-16 20:41:01 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CallbackQueryHandler, CommandHandler, ConversationHandler, MessageHandler, Filters
+from telegram.ext import Updater, CallbackQueryHandler, CommandHandler, MessageHandler, Filters
 #import os
 #PORT = int(os.environ.get('PORT', '5000'))
-
-GENDER, PHOTO, LOCATION, BIO = range(4)
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,9 +13,9 @@ TOKEN = '5939028652:AAE9hFyIrdx0gQPoMTnasEzfNGKc0P2ZVfc'
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-#def start(update, context):
-#    """Send a message when the command /start is issued."""
-#    update.message.reply_text('Hi! Hit "/" for available commands!')
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! Hit "/" for available commands!')
 
 def help(update, context):
     """Send a message when the command /help is issued."""
@@ -27,13 +25,18 @@ def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
 
-async def report(update, context) -> None:
+async def report(update, context):
     """Echo the user message."""
-    button1 = KeyboardButton("Hello")
-    button2 = KeyboardButton("Youtube")
-    keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(button1).add(button2)
+    keyboard = [
+        [
+            InlineKeyboardButton("Option 1", callback_data="1"),
+            InlineKeyboardButton("Option 2", callback_data="2"),
+        ],
+        [InlineKeyboardButton("Option 3", callback_data="3")],
+    ]
 
-    await update.message.reply("Please choose:", reply_markup=keyboard1)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text("Please choose:", reply_markup=reply_markup)
 
 async def button(update, context) -> None:
     """Parses the CallbackQuery and updates the message text."""
@@ -49,102 +52,32 @@ def error(update, context):
     """Log Errors caused by Updates."""
     update.message.reply_text(update.message.text)
 
-async def start(update , context) -> int:
-    """Starts the conversation and asks the user about their gender."""
-    reply_keyboard = [["Boy", "Girl", "Other"]]
+def fault(update, context):
+    update.message.reply_text("Please state your name: ")
+    userName = update.message.text
 
-    await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-        ),
-    )
-
-    return GENDER
-
-
-async def gender(update, context) -> int:
-    """Stores the selected gender and asks for a photo."""
-    user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text(
-        "I see! Please send me a photo of yourself, "
-        "so I know what you look like, or send /skip if you don't want to.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
-
-    return PHOTO
+    update.message.reply_text("Please state your mobile number: ")
+    userNo = update.message.text
+    if userNo == "END":
+        if endFault:
+            return
+    update.message.reply_text("Please select type of fault: ")
+    userType = update.message.text
+    update.message.reply_text("Please give a brief description of the fault: ")
+    userDesc = update.message.text
+    update.message.reply_text("(Optional) Please upload a photo of the fault: ")
+    userPhoto = update.message.text
+    fullInfo = userName + userNo + userType + userDesc + userNo
+    update.message.reply_text(fullInfo)
 
 
-async def photo(update, context) -> int:
-    """Stores the photo and asks for a location."""
-    user = update.message.from_user
-    photo_file = await update.message.photo[-1].get_file()
-    await photo_file.download_to_drive("user_photo.jpg")
-    logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
-    await update.message.reply_text(
-        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
-    )
+def filterYES(self,message):
+    return message.text == "YES" 
+    
 
-    return LOCATION
-
-
-async def skip_photo(update, context) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    await update.message.reply_text(
-        "I bet you look great! Now, send me your location please, or send /skip."
-    )
-
-    return LOCATION
-
-
-async def location(update, context) -> int:
-    """Stores the location and asks for some info about the user."""
-    user = update.message.from_user
-    user_location = update.message.location
-    logger.info(
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-    )
-    await update.message.reply_text(
-        "Maybe I can visit you sometime! At last, tell me something about yourself."
-    )
-
-    return BIO
-
-
-async def skip_location(update, context) -> int:
-    """Skips the location and asks for info about the user."""
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    await update.message.reply_text(
-        "You seem a bit paranoid! At last, tell me something about yourself."
-    )
-
-    return BIO
-
-
-async def bio(update, context) -> int:
-    """Stores the info about the user and ends the conversation."""
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text("Thank you! I hope we can talk again some day.")
-
-    return ConversationHandler.END
-
-
-async def cancel(update, context) -> int:
-    """Cancels and ends the conversation."""
-    user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
-    await update.message.reply_text(
-        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
-    )
-
-    return ConversationHandler.END
+def endFault(update, context):
+    update.message.reply_text("Discard fault report?")
+    filterYES
 
 def main():
     """Start the bot."""
@@ -157,30 +90,14 @@ def main():
     dp = updater.dispatcher
 
     # on different commands - answer in Telegram
-    #dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CallbackQueryHandler(button))
     dp.add_handler(CommandHandler("report", report))
 
-    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            GENDER: [MessageHandler(Filters.Regex("^(Boy|Girl|Other)$"), gender)],
-            PHOTO: [MessageHandler(Filters.PHOTO, photo), CommandHandler("skip", skip_photo)],
-            LOCATION: [
-                MessageHandler(Filters.LOCATION, location),
-                CommandHandler("skip", skip_location),
-            ],
-            BIO: [MessageHandler(Filters.TEXT & ~Filters.COMMAND, bio)],
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
-
-    dp.add_handler(conv_handler)
-
+    dp.add_handler(CommandHandler("fault", fault))
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    # dp.add_handler(MessageHandler(filterEND, endFault))
 
     # log all errors
     dp.add_error_handler(error)
